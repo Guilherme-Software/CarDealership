759281fb1d7aba9ea527ce69ab53d4f26966c163
Guilherme-Software <guilherme.lima.programador@gmail.com>
2025-05-20 23:24:48 -0300

    test

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # read data
-car_info = pd.read_csv("car_prices.csv")
+car_data = pd.read_csv("car_prices.csv")
 
 # creating plots
 fig, ax = plt.subplots()
 
 # selecting 10 best sell brands
-array = car_info['make'].value_counts()
+array = car_data['make'].value_counts()
 best_sell_brands = array.head(10)
 
 # Setting bars configs
-bar_colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+
 ax.set_title('Top 10 Best Sales Brands')
 ax.set_ylabel('SALES')
 
-ax.bar(best_sell_brands.index, best_sell_brands.values, color=bar_colors)
+ax.bar(best_sell_brands.index, best_sell_brands.values)
 
 plt.show()
